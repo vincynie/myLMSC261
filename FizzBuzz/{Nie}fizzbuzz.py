e3d70983945ce96e572621ca2c59206ec4ff6d04
vincynie <65456388+vincynie@users.noreply.github.com>
2021-03-15 20:37:17 -0400

    update

--- FizzBuzz/{Nie}fizzbuzz.py
+++ FizzBuzz/{Nie}fizzbuzz.py
@@ -1,9 +1,9 @@
 for number in range (1, 101):
-    if number % 3 == 0:
+    if number % 15 == 0:
+        print ("FizzBuzz")
+    elif number % 3 == 0:
         print ("Fizz")
     elif number % 5 == 0:
         print ("Buzz")
-    elif number % 15 == 0:
-        print ("FizzBuzz")
     else:
         print (number)
